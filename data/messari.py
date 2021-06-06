e4197d2665bf8eddc3bdeae9ab10b473ef0b829c
Yunus <yecicen@gmail.com>
2021-06-06 23:58:26 +0300

    fixing nomics request

--- data/messari.py
+++ data/messari.py
@@ -2,18 +2,16 @@ import requests
 import time
 from datetime import datetime as dt
 import csv
-import config
 
-NOMICS_URL = f'https://api.nomics.com/v1/currencies/ticker?key={config.getApiKey("nomics")}&ids=BTC&interval=1d&convert=USD&per-page=100&page=1'
-
+URL = "https://data.messari.io/api/v1/assets/btc/metrics"
 counter = 0
-with open('nomics.csv', mode='a') as csv_file:
+with open('messari.csv', mode='a') as csv_file:
     fieldnames = ['price_timestamp', 'price']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
     writer.writeheader()
     while True:
-        r = requests.get(url = NOMICS_URL) 
+        r = requests.get(url = URL) 
         json_data  = r.json()
         writer.writerow(
             {
@@ -24,3 +22,4 @@ with open('nomics.csv', mode='a') as csv_file:
         time.sleep(60.0)
         if counter == 3:
             break
+
